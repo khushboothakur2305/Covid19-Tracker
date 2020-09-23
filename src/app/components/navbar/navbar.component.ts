c5043874ee7b741ab4d3d642587101527f535e27
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-09-23 23:53:40 +0530

    update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,23 +10,23 @@ export class NavbarComponent implements OnInit {
   constructor(private http: HttpClient) {}
   aray: any[] = [];
   ngOnInit(): void {
-    let header = new HttpHeaders({
-      "x-rapidapi-host": "livescore6.p.rapidapi.com",
-      "x-rapidapi-key": "0afffbcfb4msh151e32a1bd8a873p133d65jsn392ba09aba88",
-      useQueryString: "true"
-    });
-    this.http
-      .get(
-        "https://livescore6.p.rapidapi.com/news/list?category=cricket",
-        {
-          headers: header,
-        }
-      )
-      .subscribe((re) => {
-        console.log(re);
-      },ree=>{
-        console.log(ree);
+    // let header = new HttpHeaders({
+    //   "x-rapidapi-host": "livescore6.p.rapidapi.com",
+    //   "x-rapidapi-key": "0afffbcfb4msh151e32a1bd8a873p133d65jsn392ba09aba88",
+    //   useQueryString: "true"
+    // });
+    // this.http
+    //   .get(
+    //     "https://livescore6.p.rapidapi.com/news/list?category=cricket",
+    //     {
+    //       headers: header,
+    //     }
+    //   )
+    //   .subscribe((re) => {
+    //     console.log(re);
+    //   },ree=>{
+    //     console.log(ree);
 
-      });
+    //   });
   }
 }
